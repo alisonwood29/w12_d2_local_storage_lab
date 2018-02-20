bde86b1050b5df0443da8992e181f10e09bf7f0d
Alison Wood <alisonwood29@gmail.com>
2018-02-20 14:29:09 +0000

    added populate function

--- public/app.js
+++ public/app.js
@@ -9,41 +9,47 @@ var init = function () {
 var populate = function (todos) {
   // this function needs to:
   // - loop through the array of todos, invoking addItem() for each todo item
+  for(item of todos){
+    addItem(item);
+  }
 }
 
 var addItem = function (item) {
   var ul = document.querySelector('#todo-list');
   // this function needs to:
   // - create an li element containing the string 'item'
-  const listItem = document.createElement('li');
-  listItem.innerText = item;
+  const toDoListItem = document.createElement('li');
+  toDoListItem.innerText = item;
   // - append the li to the "todo-list" ul
-  ul.appendChild(listItem);
+  ul.appendChild(toDoListItem);
 }
 
 var handleButtonClick = function () {
   // this function needs to:
   // - get hold of the input box's value
-  const input = document.querySelector('#new-item').value;
+  const input = document.querySelector('#new-item');
+  const value = input.value;
   console.log(input);
   // - append it to the "todo-list" ul by invoking addItem()
-  addItem(input);
+  addItem(value);
   // - add it to local storage by invoking save()
-  save(input);
+  save(value);
+
+  input.value = '';
 }
 
 var save = function (newItem) {
 
 // - get the data back from local storage and parse to an array
-  jsonToDoList = localStorage.getItem('todolist');
-  todolist = JSON.parse(jsonToDoList) || [];
-  todolist.push(newItem);
-  console.log(todolist);
+  const savedJSONToDoList = localStorage.getItem('todoList');
+  const savedToDoList = JSON.parse(savedJSONToDoList) || [];
+  savedToDoList.push(newItem);
+  // console.log(todolist);
   // - add the newItem to the array
   // - stringify the updated array
-  newJsonToDoList = JSON.stringify(todolist);
+  const updatedJSONToDoList = JSON.stringify(savedToDoList);
   // - save it back to localstoage
-  localStorage.setItem('todolist', newJsonToDoList);
+  localStorage.setItem('todoList', updatedJSONToDoList);
 }
 
-window.addEventListener('load', init);
+document.addEventListener('DOMContentLoaded', init);
